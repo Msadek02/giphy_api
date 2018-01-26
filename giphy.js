ca0d99f2f69e41e449ef031cb3374187c90a4f44
Msadek02 <Thunder_axe_90@hotmail.com>
2018-01-26 16:46:27 -0500

    looping through the gifs and play and stop it when mouseover

---
+++ giphy.js
@@ -0,0 +1,86 @@
+  $(document).ready(function() {
+      var gifs = ["cats", "dogs", "husky"];
+
+      function displayGifs() {
+          var gif = $(this).attr("data-name");
+          var queryURL = "http://api.giphy.com/v1/gifs/search?q= " + gif + "&api_key=riFcpySJBkJmW8iczsascuMVH9GjPM1W&limit=5";
+
+
+          $.ajax({
+              url: queryURL,
+              method: 'GET'
+          }).done(function(response) {
+
+              var results = response.data;
+
+              for (var i = 0; i < results.length; i++) {
+
+                  var gifDiv = $("<div>");
+
+                  var image = $("<img class='gif'>");
+
+                  image.attr("src", results[i].images.fixed_width_still.url);
+
+                  image.attr("data-animate", results[i].images.fixed_width.url);
+
+                  image.attr("data-still", results[i].images.fixed_width_still.url);
+
+                  image.attr("data-state", "still");
+
+                  image.append(image);
+
+                  $("#gif-view").append(image);
+
+              }
+
+              $(".gif").on("mouseover", function() {
+
+                  var state = $(this).attr("data-state");
+
+                  if (state === "still") {
+                      $(this).attr("src", $(this).attr("data-animate"));
+                      $(this).attr("data-state", "data-animate");
+                  } else {
+                      $(this).attr("src", $(this).attr("data-still"));
+                      $(this).attr("data-state", "still");
+                  }
+              });
+
+          });
+      }
+
+      function renderButtons() {
+
+          $("#buttons-view").empty();
+
+          for (var i = 0; i < gifs.length; i++) {
+
+              var a = $("<button>");
+
+              a.addClass("gif-button");
+
+              a.attr("data-name", gifs[i]);
+
+              a.text(gifs[i]);
+
+              $("#buttons-view").append(a);
+          }
+      }
+
+
+      $("#add-gif").on("click", function(event) {
+
+          event.preventDefault();
+
+          var gif = $("#gif-input").val().trim();
+
+          gifs.push(gif);
+
+          renderButtons();
+      });
+
+      $(document).on("click", ".gif-button", displayGifs);
+
+      renderButtons();
+
+  });
