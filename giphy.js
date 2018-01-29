5397e72510c604eb18815f5f2c90544bde54b6b2
Msadek02 <Thunder_axe_90@hotmail.com>
2018-01-29 15:00:31 -0500

    cleaning up for better reading

--- giphy.js
+++ giphy.js
@@ -1,86 +1,86 @@
   $(document).ready(function() {
-      var players = ["Mohamed Salah", "Zlatan", "Ronaldo","Messi","Ronaldinho"];
+  var players = ["Mohamed Salah", "Zlatan", "Ronaldo","Messi","Ronaldinho"];
 
-      function displayPlayers() {
-          var gif = $(this).attr("data-name");
-          var queryURL = "https://api.giphy.com/v1/gifs/search?q= " + gif + "&api_key=riFcpySJBkJmW8iczsascuMVH9GjPM1W&limit=10";
+  function displayPlayers() {
+      var gif = $(this).attr("data-name");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q= " + gif + "&api_key=riFcpySJBkJmW8iczsascuMVH9GjPM1W&limit=10";
 
 
-          $.ajax({
-              url: queryURL,
-              method: 'GET'
-          }).done(function(response) {
+      $.ajax({
+          url: queryURL,
+          method: 'GET'
+      }).done(function(response) {
 
-              var results = response.data;
+          var results = response.data;
 
-              for (var i = 0; i < results.length; i++) {
+          for (var i = 0; i < results.length; i++) {
 
-                  var gifDiv = $("<div>");
+              var gifDiv = $("<div>");
 
-                  var image = $("<img class='gif'>");
+              var image = $("<img class='gif'>");
 
-                  image.attr("src", results[i].images.fixed_width_still.url);
+              image.attr("src", results[i].images.fixed_width_still.url);
 
-                  image.attr("data-animate", results[i].images.fixed_width.url);
+              image.attr("data-animate", results[i].images.fixed_width.url);
 
-                  image.attr("data-still", results[i].images.fixed_width_still.url);
+              image.attr("data-still", results[i].images.fixed_width_still.url);
 
-                  image.attr("data-state", "still");
+              image.attr("data-state", "still");
 
-                  image.append(image);
+              image.append(image);
 
-                  $("#gif-view").prepend(image);
+              $("#gif-view").prepend(image);
 
-              }
-
-              $(".gif").on("click", function() {
-
-                  var state = $(this).attr("data-state");
-
-                  if (state === "still") {
-                      $(this).attr("src", $(this).attr("data-animate"));
-                      $(this).attr("data-state", "data-animate");
-                  } else {
-                      $(this).attr("src", $(this).attr("data-still"));
-                      $(this).attr("data-state", "still");
-                  }
-              });
-
-          });
-      }
-
-      function renderButtons() {
-
-          $("#buttons-view").empty();
-
-          for (var i = 0; i < players.length; i++) {
-
-              var a = $("<button type='button' class='btn btn-primary'>");
-
-              a.addClass("gif-button");
-
-              a.attr("data-name", players[i]);
-
-              a.text(players[i]);
-
-              $("#buttons-view").append(a);
           }
-      }
 
+          $(".gif").on("click", function() {
 
-      $("#add-gif").on("click", function(event) {
+              var state = $(this).attr("data-state");
 
-          event.preventDefault();
+              if (state === "still") {
+                  $(this).attr("src", $(this).attr("data-animate"));
+                  $(this).attr("data-state", "data-animate");
+              } else {
+                  $(this).attr("src", $(this).attr("data-still"));
+                  $(this).attr("data-state", "still");
+              }
+          });
 
-          var gif = $("#gif-input").val().trim();
-
-          players.push(gif);
-
-          renderButtons();
       });
+  }
 
-      $(document).on("click", ".gif-button", displayPlayers);
+  function renderButtons() {
+
+      $("#buttons-view").empty();
+
+      for (var i = 0; i < players.length; i++) {
+
+          var a = $("<button type='button' class='btn btn-primary'>");
+
+          a.addClass("gif-button");
+
+          a.attr("data-name", players[i]);
+
+          a.text(players[i]);
+
+          $("#buttons-view").append(a);
+      }
+  }
+
+
+  $("#add-gif").on("click", function(event) {
+
+      event.preventDefault();
+
+      var gif = $("#gif-input").val().trim();
+
+      players.push(gif);
 
       renderButtons();
+  });
+
+  $(document).on("click", ".gif-button", displayPlayers);
+
+  renderButtons();
 
   });
